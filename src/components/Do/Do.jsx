d75fe13ef76046d1843ea3536025f2d0d5bcb66d
Schynol-dev <kamil.schynol0dev@gmail.com>
2021-03-23 23:30:27 +0100

    Do Component - first stage

---
+++ src/components/Do/Do.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+function Do() {
+
+    return (
+        <section id="do" className="wrapper style2">
+            <div className="container">
+                <div className="boxes">
+                    <div className="box">
+                        <div className="image"></div>
+                        <div className="text">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa obcaecati inventore temporibus et delectus officia quaerat ex possimus magni quam commodi,
+                            blanditiis nam aperiam qui fugiat repellendus iusto tempora voluptas!</p>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <div className="image"></div>
+                        <div className="text">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa obcaecati inventore temporibus et delectus officia quaerat ex possimus magni quam commodi,
+                            blanditiis nam aperiam qui fugiat repellendus iusto tempora voluptas!</p>
+                        </div>
+                    </div> 
+                    <div className="box">
+                        <div className="image"></div>
+                        <div className="text">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa obcaecati inventore temporibus et delectus officia quaerat ex possimus magni quam commodi,
+                            blanditiis nam aperiam qui fugiat repellendus iusto tempora voluptas!</p>
+                        </div>
+                    </div> 
+                    <div className="box">
+                        <div className="image"></div>
+                        <div className="text">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa obcaecati inventore temporibus et delectus officia quaerat ex possimus magni quam commodi,
+                            blanditiis nam aperiam qui fugiat repellendus iusto tempora voluptas!</p>
+                        </div>
+                    </div>     
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Do;
