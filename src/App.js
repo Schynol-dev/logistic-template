55ba72ae945b15fb19782c6a9ee6fd635e7e05a1
Schynol-dev <kamil.schynol0dev@gmail.com>
2021-03-23 22:05:04 +0100

    Navigation Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import "./assets/css/main.css";
 
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
-    <></>
+    <>
+      <Navigation />
+    </>
   );
 }
 
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Navigation() {
+
+    return (
+        <div id="navigation">
+            <div className="logo">
+                <h3>Logistic</h3>
+            </div>
+            <div className="list">
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#do">What We Do</a></li>
+                    <li><a href="#abour">About Us</a></li>
+                    <li><a href="#contact">Hire Us</a></li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Navigation;
