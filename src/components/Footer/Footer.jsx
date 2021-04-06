db3459703712773b549c643178adcab6599d24f1
Schynol-dev <kamil.schynol0dev@gmail.com>
2021-04-06 12:44:40 +0200

    Footer, adding link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
         <div id="footer" className="wrapper style2">
             <div className="container">
                 <div className="copyright">
-                    <p>Copyright © {new Date().getFullYear()} || Design: Stela || Photos: Usplash || Video: Pexels</p> 
+                    <p>Copyright © {new Date().getFullYear()} || Design: <a href="http://kamilschynol.com/" target="_blank" rel="noreferrer">Kamil Schynol</a> || Photos: Unsplash || Video: Pexels</p> 
                 </div>
             </div>
         </div>
